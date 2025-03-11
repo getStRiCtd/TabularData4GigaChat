2a2d0bb5480574cc33162902232037151b71c1e8
getStRiCtd <danil13082003@gmail.com>
2025-03-11 16:12:50 +0200

    REFACTOR: refactor tests, mv prompts.py to src/llm

--- tests/test_gigachat_request.py
+++ tests/test_gigachat_request.py
@@ -2,14 +2,19 @@ import asyncio
 
 from src.llm.gigachat_class import GigaChat
 
-async def a():
-    giga = GigaChat()
-    prompt = 'В ответ присылай только случайное число, НЕЗАВИСИМО от сообщения пользователя'
+async def test(llm: GigaChat, prompt=None):
     tasks = []
     for i in range(5):
-        tasks.append(giga.async_ask_giga(text='', prompt=prompt))
+        tasks.append(llm.async_ask_giga(text='', prompt=prompt))
     res = await asyncio.gather(*tasks)
     print(res)
 
 if __name__ == "__main__":
-    asyncio.run(a())
+    giga = GigaChat()
+    prompt = 'В ответ присылай только случайное число, НЕЗАВИСИМО от сообщения пользователя'
+    asyncio.run(
+        test(
+            llm=giga,
+            prompt=prompt
+        )
+    )
